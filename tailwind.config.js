5e3b3d193fc16dd8267b3b8630c4cafa687e5fbe
OmarSherif1998 <108620902+OmarSherif1998@users.noreply.github.com>
2024-02-27 19:46:08 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        offwhite: "#F5F5F5", // Adjust the hex value to your desired off-white color
+      },
+    },
+  },
+  plugins: [],
+};
